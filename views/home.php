dd2edabe798118aa77d69c7d91bb7d22c1540bca
DanielAbabu <Daniel0Ababu@gmail.com>
2024-05-27 17:02:38 +0300

    book display

--- views/home.php
+++ views/home.php
@@ -9,14 +9,60 @@
 </head>
 
 <body>
-    <?php require "partials/header.php" ?>
+    <?php require "partials/header.php";
+    $books = [
+        [
+            "poster" => "front.png",
+            "title" => "Advanced programming",
+            "author" => "Daniel Ababu",
+            "amount" => 3
+        ],
+        [
+            "poster" => "front.png",
+            "title" => "Advanced programming",
+            "author" => "Daniel Ababu",
+            "amount" => 3
+        ],
+        [
+            "poster" => "front.png",
+            "title" => "Advanced programming",
+            "author" => "Daniel Ababu",
+            "amount" => 3
+        ],
+        [
+            "poster" => "front.png",
+            "title" => "Advanced programming",
+            "author" => "Daniel Ababu",
+            "amount" => 3
+        ],
+        [
+            "poster" => "front.png",
+            "title" => "Advanced programming",
+            "author" => "Daniel Ababu",
+            "amount" => 3
+        ]
+    ]
+    ?>
 
-    <section id="search">
-        <h2>Search for Books</h2>
-        <form action="searchview.php" method="get">
-            <input type="text" name="query" id="sinput" placeholder="Search by title, author, or ISBN">
-            <button type="submit">Search</button>
-        </form>
+    <section>
+        <div id="search">
+            <h2>Search for Books</h2>
+            <form action="searchview.php" method="get">
+                <input type="text" name="query" id="sinput" placeholder="Search by title, author, or ISBN">
+                <button type="submit">Search</button>
+            </form>
+        </div>
+
+        <div id="books">
+            <?php foreach ($books as $book) : ?>
+                <div class="card">
+                    <div class="cardimg"><img src="<?= $book["poster"] ?>" alt="book image"></div>
+                    <h4 class="ctitle"><?= $book["title"] ?></h4>
+                    <p class="cauthor"> <?= $book["author"] ?></p>
+                    <p class="camount"><?= $book["amount"] ?></p>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </section>
 
 
